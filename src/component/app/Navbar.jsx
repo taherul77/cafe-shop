b8961617d9a24053627ea433f169869aa1f4e764
taherul77 <taherul2317@gmail.com>
2023-09-01 19:39:19 +0600

    update project

--- src/component/app/Navbar.jsx
+++ src/component/app/Navbar.jsx
@@ -3,7 +3,7 @@ import logo from "../../assets/icons/star.png";
 
 const Header = () => {
   return (
-    <div>
+    <div className="bg-white">
       <div className="navbar py-5 lg:px-20 pe-5">
         <div className="navbar-start">
           <div className="dropdown">
@@ -27,56 +27,44 @@ const Header = () => {
               tabIndex={0}
               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
             >
-              <Link to="/our-shop">
+              <Link to="/">
                 <li>
-                  <a>Our Shop</a>
+                  <a className="text-black">Home</a>
                 </li>
               </Link>
+              <Link to="/our-shop">
+                <li>
+                  <a className="text-black">Our Shop</a>
+                </li>
+              </Link>
+             
               <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
+                <a className="text-black">Blog</a>
               </li>
             </ul>
           </div>
           <Link to="/">
             <div className="flex gap-2">
               <img src={logo} alt="" className="h-8" />
-              <a className="lg:text-2xl text-lg text-white">S T R A D A L A</a>
+              <a className="lg:text-2xl text-lg text-black">S T R A D A L A</a>
             </div>
           </Link>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
-            <Link to="/our-shop">
+            <Link to="/">
               <li>
-                <a>Our Shop</a>
+                <a className="text-black">Home</a>
               </li>
             </Link>
-            <li tabIndex={0}>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
+            <Link to="/our-shop">
+              <li>
+                <a className="text-black">Our Shop</a>
+              </li>
+            </Link>
+           
             <li>
-              <a>Item 3</a>
+              <a className="text-black">Blog</a>
             </li>
           </ul>
         </div>
